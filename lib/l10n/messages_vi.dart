5d4dcf9a056ba0c1f90054e84673015c8ffe595c
Minh Kiet <kietdk@gmail.com>
2019-03-18 09:17:04 +0700

    second

---
+++ lib/l10n/messages_vi.dart
@@ -0,0 +1,23 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a es locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = MessageLookup();
+
+final _keepAnalysisHappy = Intl.defaultLocale;
+
+typedef MessageIfAbsent(String message_str, List args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  get localeName => 'vi';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => {
+    "title" : MessageLookupByLibrary.simpleMessage("Xin chào xx"),
+    "title2" : MessageLookupByLibrary.simpleMessage("Xin chào xx 2")
+  };
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
+
+
+import 'l10n/messages_all.dart';
+
+class DemoLocalizations {
+  static Future<DemoLocalizations> load(Locale locale) {
+    final String name = locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
+    final String localeName = Intl.canonicalizedLocale(name);
+
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      return DemoLocalizations();
+    });
+  }
+
+  static DemoLocalizations of(BuildContext context) {
+    return Localizations.of<DemoLocalizations>(context, DemoLocalizations);
+  }
+
+  String get title {
+    return Intl.message(
+      'Hello World Main',
+      name: 'title',
+      desc: 'Title for the Demo application',
+    );
+  }
+  String get title2 {
+    return Intl.message(
+      'Hello World Main',
+      name: 'title2',
+      desc: 'Title for the Demo application',
+    );
+  }
+}
+
+class DemoLocalizationsDelegate extends LocalizationsDelegate<DemoLocalizations> {
+  const DemoLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'vi'].contains(locale.languageCode);
+
+  @override
+  Future<DemoLocalizations> load(Locale locale) => DemoLocalizations.load(locale);
+
+  @override
+  bool shouldReload(DemoLocalizationsDelegate old) => false;
+}
+
+class DemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(DemoLocalizations.of(context).title2),
+      ),
+      body: Center(
+        child: Text(DemoLocalizations.of(context).title),
+      ),
+    );
+  }
+}
+
+class Demo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateTitle: (BuildContext context) => DemoLocalizations.of(context).title,
+      localizationsDelegates: [
+        const DemoLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('vi', ''),
+      ],
+      home: DemoApp(),
+    );
+  }
+}
+
+void main() {
+  runApp(Demo());
+}
